5c247e3808bc2090892a1e0edcf60b5e03e21f37
sphull209 <sagar.phull7@gmail.com>
2025-02-11 19:43:46 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,73 @@
+variable "aws_region" {
+  type = string
+  description = "aws region of our architecture"
+}
+
+variable "azs" {
+  type = list(string)
+  description = "list of availability zones"
+}
+
+variable "Keel_vpc_cidr" {
+  type = string
+  description = "cidr block of Keel vpc"
+}
+
+variable "subnets_cidrs" {
+  type = list(string)
+  description = "List of CIDR blocks for subnets (web, app, and db tiers)"
+}
+
+variable "key_name" {
+  type = string
+  description = "aws key pair name"
+}
+
+variable "ami_value" {
+  type = string
+  description = "ami id used for autoscaling groups"
+}
+
+variable "instance_type" {
+  type = string
+  description = "instance type for autoscaling groups "
+}
+
+variable "db_username" {
+  type = string
+  description = "Keel postgressql db username"
+}
+
+variable "db_password" {
+  type = string
+  description = "Keel postgressql db password"
+}
+
+variable "db_engine" {
+  type = string
+  description = "Keel postgressql db engine type"
+}
+
+variable "db_engine_version" {
+  type = string
+  description = "Keel postgressql db engine version"
+}
+
+variable "db_instance_class" {
+  type = string
+  description = "Keel postgressql db instance class(type)"
+}
+
+variable "db_identifier" {
+  type = string
+  description = "Keel postgressql db identifier"
+}
+
+variable "access_ip" {
+  type = string
+  description = "sepcific ip address only permit for ssh into bastion instance"
+}
+
+variable "s3_artifact_bucket" {
+  description = "Name of the S3 bucket for artifact storage"
+}
