ed8b72a5acb259547849768945f11b2692a0cefe
sphull209 <sagar.phull7@gmail.com>
2025-02-14 07:51:05 -0500

    Update install_apache.sh

--- install_apache.sh
+++ install_apache.sh
@@ -5,7 +5,7 @@ mkdir flask-app
 cd flask-app
 ######## Copy app.py and index.html to flask-app directory #############
 cp index.html /flask-app/
-
+cp app.py /flask-app/
 python3 -m venv venv
 source venv/bin/activate
 
