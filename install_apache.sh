3f2fc3dfb595e521e2417822e82ee3eccca8525f
sphull209 <sagar.phull7@gmail.com>
2025-02-14 07:49:54 -0500

    Update install_apache.sh

--- install_apache.sh
+++ install_apache.sh
@@ -3,6 +3,9 @@ yum update -y
 yum install python3 -y
 mkdir flask-app
 cd flask-app
+######## Copy app.py and index.html to flask-app directory #############
+cp index.html /flask-app/
+
 python3 -m venv venv
 source venv/bin/activate
 
@@ -12,3 +15,4 @@ pip install pipenv
 pipenv --version
 pip install psycopg2-binary
 pipenv install psycopg2
+python app.py
