d33cd619aca9d329f50bce88860a9b5b15057992
sphull209 <sagar.phull7@gmail.com>
2025-02-16 17:46:24 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,8 @@ output "load_balancer_endpoint" {
 output "db_endpoint" {
   value = module.database.Keel_postgressql_db_endpoint
 }
+
+output "ecs_service_name" {
+  description = "The ECS service name"
+  value       = module.ecs_task.ecs_service_name
+}
